d7abff68bb8ad6d0e0f2124fab9b80edc02aa8b5
mazhiyuan <mazhiyuan@megalinkware.com>
2024-11-01 17:49:31 +0800

    setup proxy

--- ai/index.ts
+++ ai/index.ts
@@ -1,7 +1,13 @@
-import { openai } from '@ai-sdk/openai';
+import { createOpenAI } from '@ai-sdk/openai';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
+
+export const openai = createOpenAI({
+  // custom settings, e.g.
+  compatibility: 'strict', // strict mode, enable when using the OpenAI API
+  baseURL: process.env.AI_BASE_URL,
+});
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
